32b719d74aa0318a801d83c25c12ff5918601f51
Mattias Erming <mattias@mattiaserming.com>
2014-04-11 16:41:42 +0200

    Initial commit

---
+++ jquery.inputHistory.js
@@ -0,0 +1,15 @@
+/*!
+ * jquery-input-history
+ * https://github.com/erming/jquery-input-history
+ *
+ * Copyright (c) 2014 Mattias Erming <mattias@mattiaserming.com>
+ * Licensed under the MIT License.
+ *
+ * Version 0.0.1
+ */
+
+(function($) {
+	$.fn.inputHistory = function() {
+		// ..
+	}
+})(jQuery);
