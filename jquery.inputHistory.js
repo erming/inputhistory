023a671407e5286ca2ee715b21b7251702fa8c23
Mattias Erming <mattias@mattiaserming.com>
2014-04-25 17:05:17 +0200

    Minor changes

--- jquery.inputHistory.js
+++ jquery.inputHistory.js
@@ -7,11 +7,6 @@
  *
  * Version 0.1.1
  */
- 
-if (typeof jQuery === 'undefined') {
-	throw new Error('inputHistory requires jQuery')
-}
-
 (function($) {
 	$.fn.inputHistory = function(options) {
 		var settings = $.extend({
